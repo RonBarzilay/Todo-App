80497b60c62a7473af6350cf583d555039f94e7a
Ron Barzilay <ron.barzilay@gmail.com>
2023-01-27 15:25:35 +0200

    Styling and Basic layout

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Another Piece of Text</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Here goes the goal'/>
+        <Button title='Add Goal'/>
       </View>
-    <Text>Hello World</Text>
+      <View style={styles.goalContainer}>
+        <Text>List of goals later on ...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // flex: 1, all space for this appContainer
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+    paddingHorizontal: 16
+  },
+  inputContainer: {
+  // flex: 1/6, inputContainer:goalContainer
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    padding: 10, 
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8
+  },
+  // flex: 5/6, goalContainer:inputContainer
+  goalContainer: {
+    flex: 5,
   },
 });
